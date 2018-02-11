718cf252384cc95890bedc2b1fa5d7701f4d7e89
Aaron Arinder <aaron@docnetwork.org>
2018-02-11 13:38:57 -0500

    OsmosiS: added fs

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
 const osmosis = require('osmosis');
+const fs = require('fs');
 
-osmosis
-  .get('https://en.wikipedia.org/wiki/List_of_U.S._states_and_territories_by_population')
-  .find('.wikitable:first tr:gt(0)')
-  .set({
-    state: 'td[3]',
-    population: 'td[4]'
+
+const scrapePopulations = (results) => {
+  return new Promise((resolve, reject) => {
+
+  const results = [];
+
+  osmosis
+    .get('https://en.wikipedia.org/wiki/List_of_U.S._states_and_territories_by_population')
+    .find('.wikitable:first tr:gt(0)')
+    .set({
+      state: 'td[3]',
+      population: 'td[5]'
+    })
+    .follow('td[3] a@href')
+    .set({
+      longitude: '.longitude',
+      latitude: '.latitude'
+    })
+    .data(data => results.push(data))
+    .done(() => resolve(results))
   })
-  .follow('td[3] a@href')
-  .set({
-    longitude: '.longitude',
-    latitude: '.latitude'
-  })
-  .data(data => console.log(data))
+}
+
+scrapePopulations().then((data, err) => {
+
+  fs.writeFile('data-scraped.json', JSON.stringify(data, null, 4),
+    function(err){
+      if(err) console.error(err)
+      else console.log('data saved')
+    })
+  }
+)
